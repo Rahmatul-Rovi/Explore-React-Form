8c7eb7f937165276f75a2d1f8f0cb0f342cd84ec
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2025-11-28 14:26:48 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1>Explore React Form</h1>
+      
+    <div>
+     <SimpleForm></SimpleForm>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SimpleForm = () => {
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+    }
+    return (
+        <div>
+           <form onSubmit={handleSubmit}>
+             <input type="text" name='name' placeholder='Your Name' />
+             <br />
+             <input type="email" placeholder='Your Email' />
+            <input type="button" value="Submit" />
+           </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
